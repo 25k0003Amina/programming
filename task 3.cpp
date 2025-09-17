c34be1de7a9687f9734ab8fa08f0cefb5fd62380
25k0003Amina <k250003@nu.edu.pk>
2025-09-17 08:38:39 -0700

    updated commit

---
+++ task 3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Enter your first number between 65 and 90 = ");
+    
+    scanf ("%d",&num);
+    
+     printf ("%d = %c",num,num);
+     
+    
+     return 0;
+}
+
